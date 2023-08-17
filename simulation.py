bca55553360ec4fec06f1a1a83a5cac47123e457
zhaoz0314 <92612715+zhaoz0314@users.noreply.github.com>
2023-08-17 13:28:49 -0400

    Add files via upload

--- simulation.py
+++ simulation.py
@@ -1,15 +1,15 @@
+# with the default parameters, the estimated run time is 15 hours on free colab
 import msprime
 from pathlib import Path
 
 # set parameters
-# with the default parameters, the estimated run time is 15 hours
 trial_number= 90000 # num of trials
 v_bin_number= 200 # num of bins
 sample_size=2 # num of individuals
 v_ploidy=1 # num of chromosomes of each type
-combined_parameters= [0, 1/4, 1/2, 1, 2, 4, 8, 16, 20, 24, 28, 32] # sequence_length*recombination_rate*population_size
-sequence_lengths= [100*10**idx for idx in range(3)] # sequence length
-population_sizes= [1000*10**idx for idx in range(3)] # population size
+combined_parameters= [300, 100, 30, 10, 1, 0] # sequence_length*recombination_rate*population_size
+sequence_lengths= [100] # sequence length
+population_sizes= [round(10*10**(idx/4)) for idx in range(9)] # population size
 parameter_packages= [[combined_parameter, v_sequence_length, v_population_size,
                       combined_parameter/v_sequence_length/v_population_size]
                      for combined_parameter in combined_parameters
@@ -20,9 +20,13 @@ parameter_packages= [[combined_parameter, v_sequence_length, v_population_size,
 
 # define the function that finds the least coalescent time
 def mrca_time_api(tree_sequence):
-    ca_times= []
+    # ca_times= []
+    ca_times= [0. for idx in range(tree_sequence.num_trees)]
+    tree_counter= 0
     for tree in tree_sequence.trees():
-        ca_times.append(tree_sequence.tables.nodes[tree.root].time)
+        # ca_times.append(tree_sequence.tables.nodes[tree.root].time)
+        ca_times[tree_counter]= tree_sequence.tables.nodes[tree.root].time
+        tree_counter= tree_counter+ 1
     return(min(ca_times))
 
 # define the function that transforms lists to histograms
@@ -39,7 +43,7 @@ def list_2_histogram(f_list,bin_number):
 
 # save the parameter packages
 subdirectory= ("{:.1e}".format(trial_number)+ "_"+
-           "{:.1e}".format(v_bin_number)+ "/")
+           "{:.1e}".format(v_bin_number)+ "_400/")
 Path(subdirectory).mkdir()
 with open(subdirectory+ "parameter_packages.txt", 'w') as txtfile:
     txtfile.write(str(parameter_packages))
@@ -53,7 +57,8 @@ with open(subdirectory+ "population_sizes.txt", 'w') as txtfile:
 # run the simulation trial_number times for each parameter package
 # and save each histogram
 for parameter_package in parameter_packages:
-    mrca_times= []
+    # mrca_times= []
+    mrca_times= [0. for idx in range(trial_number)]
     for idx in range(trial_number):
         tree_sequence_simulated= msprime.sim_ancestry(
             samples= sample_size,
@@ -62,7 +67,8 @@ for parameter_package in parameter_packages:
             sequence_length= parameter_package[1],
             population_size= parameter_package[2],
             record_full_arg= True)
-        mrca_times.append(mrca_time_api(tree_sequence_simulated)/parameter_package[2])
+        # mrca_times.append(mrca_time_api(tree_sequence_simulated)/parameter_package[2])
+        mrca_times[idx]= mrca_time_api(tree_sequence_simulated)/parameter_package[2]
     histogram= list_2_histogram(mrca_times, v_bin_number)
     # with open("parameter_package"+datetime.now().strftime("%H_%M_%S")+".txt", 'w') as txtfile:
     filename= ("{:.1e}".format(parameter_package[0])+ "_"+
